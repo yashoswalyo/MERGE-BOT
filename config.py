4df36983799bd0b2d9b82d07030dbc441a41270a
yashoswalyo <yashoswal18@gmail.com>
2021-10-08 17:29:32 +0530

    Final Push Added database variables to config

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 
 class Config(object):
-	API_HASH = os.getenv('API_HASH')
-	BOT_TOKEN = os.getenv('BOT_TOKEN')
-	API_ID = int(os.getenv('API_ID'))
+	API_HASH = os.getenv('API_HASH','')
+	BOT_TOKEN = os.getenv('BOT_TOKEN','')
+	API_ID = int(os.getenv('API_ID',''))
 	upload_as_doc = False
-	ALD_USR = os.getenv('ALD_USR','')
-	OWNER_USERNAME = os.getenv('OWNER_USERNAME')
+	OWNER = int(os.environ.get('OWNER','589641907'))
+	OWNER_USERNAME = os.getenv('OWNER_USERNAME','yashoswalyo')
+	PASSWORD = os.getenv('PASSWORD','mergebot')
+	DATABASE_URL=os.environ.get("DATABASE_URL","")
 	FLAG = int(os.getenv('FLAG',1))		# Dont Change this(unfinished feature!!) or else bot will stop working
 	PROGRESS = """
 Percentage : {0}%
@@ -15,4 +17,3 @@ Total: {2}
 Speed: {3}/s
 ETA: {4}
 """
-	PASSWORD = os.getenv('PASSWORD','mergebot')
