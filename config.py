f43b1b19326c773c8b0ec6bcc79bcacb9579bb78
yesraaj11 <78349719+yesraaj11@users.noreply.github.com>
2021-11-23 20:32:28 +0530

    Update

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
 	API_HASH = os.getenv('API_HASH','')
 	BOT_TOKEN = os.getenv('BOT_TOKEN','')
 	API_ID = int(os.getenv('API_ID',''))
-	OWNER = int(os.environ.get('OWNER','589641907'))
-	OWNER_USERNAME = os.getenv('OWNER_USERNAME','yashoswalyo')
+	OWNER = int(os.environ.get('OWNER',''))
+	OWNER_USERNAME = os.getenv('OWNER_USERNAME','')
 	PASSWORD = os.getenv('PASSWORD','mergebot')
 	DATABASE_URL=os.environ.get("DATABASE_URL","")
 	FLAG = int(os.getenv('FLAG',1))		# Dont Change this(unfinished feature!!) or else bot will stop working
