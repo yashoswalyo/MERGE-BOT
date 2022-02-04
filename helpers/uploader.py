0a4854b322757beb8648ab42005413e5478026f4
yashoswalyo <yashoswal18@gmail.com>
2022-02-04 23:53:43 +0530

    Send merged video to Log Channel

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -2,7 +2,7 @@ import os
 import time
 import asyncio
 from pyrogram import Client
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery,Message
 from helpers.display_progress import Progress
 from config import Config
 
@@ -13,7 +13,7 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,width,height
 		prog = Progress(cb.from_user.id,c,cb.message)
 		if upload_mode is False:
 			c_time = time.time()
-			sent_ = await c.send_video(
+			sent_:Message = await c.send_video(
 				chat_id=cb.message.chat.id,
 				video=merged_video_path,
 				height=height,
@@ -29,7 +29,7 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,width,height
 			)
 		else:
 			c_time = time.time()
-			sent_ = await c.send_document(
+			sent_:Message = await c.send_document(
 				chat_id=cb.message.chat.id,
 				document=merged_video_path,
 				thumb=video_thumbnail,
@@ -43,3 +43,10 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,width,height
 	except Exception as err:
 		print(err)
 		await cb.message.edit("Failed to upload")
+	if sent_ is not None:
+		if Config.LOGCHANNEL is not None:
+			media = sent_.video or sent_.document
+			await sent_.copy(
+				chat_id=Config.LOGCHANNEL,
+				caption=f"`{media.file_name}`\n\nMerged for: {cb.from_user.mention}",
+			)
