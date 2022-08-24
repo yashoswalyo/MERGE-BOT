616938c869e63af0450254f04c91701d8fbbac1d
mrtechnocratinsta0 <111647783+mrtechnocratinsta0@users.noreply.github.com>
2022-08-24 13:04:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = os.environ.get("API_ID", "")
     OWNER = int(os.environ.get("OWNER"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
