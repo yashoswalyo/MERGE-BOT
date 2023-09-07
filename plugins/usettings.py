c1b0801f425ceae48872ff9b1ff7fcfe4ccf72bb
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-09-07 06:56:16 +0530

    Update usettings.py

--- plugins/usettings.py
+++ plugins/usettings.py
@@ -26,28 +26,28 @@ async def userSettings(
     if usettings.user_id:
         if usettings.merge_mode == 1:
             userMergeModeId = 1
-            userMergeModeStr = "Video 🎥 + Video 🎥"
+            userMergeModeStr = "📽️Video + Video📽️"
         elif usettings.merge_mode == 2:
             userMergeModeId = 2
-            userMergeModeStr = "Video 🎥 + Audio 🎵"
+            userMergeModeStr = "📽️Video + Audio📼"
         elif usettings.merge_mode == 3:
             userMergeModeId = 3
-            userMergeModeStr = "Video 🎥 + Subtitle 📜"
+            userMergeModeStr = "📽️Video + Subtitle📜"
         elif usettings.merge_mode == 4:
             userMergeModeId = 4
-            userMergeModeStr = "Extract" 
+            userMergeModeStr = "🪢Extract🪢" 
         if usettings.edit_metadata:
-            editMetadataStr = "✅"
+            editMetadataStr = "True"
         else:
-            editMetadataStr = "❌"
+            editMetadataStr = "False"
         uSettingsMessage = f"""
 <b><u>Merge Bot settings for <a href='tg://user?id={uid}'>{fname} {lname}</a></u></b>
     ┃
-    ┣**👦 ID: <u>{usettings.user_id}</u>**
-    ┣**{'🚫' if usettings.banned else '🫡'} Ban Status: <u>{usettings.banned}</u>**
-    ┣**{'⚡' if usettings.allowed else '❗'} Allowed: <u>{usettings.allowed}</u>**
-    ┣**{'✅' if usettings.edit_metadata else '❌'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
-    ┗**Ⓜ️ Merge mode: <u>{userMergeModeStr}</u>**
+    ┣**ID: <u>{usettings.user_id}</u>**
+    ┣**{'🎍' if usettings.banned else '🎍'} Ban Status: <u>{usettings.banned}</u>**
+    ┣**{'🎭' if usettings.allowed else '🎭'} Allowed: <u>{usettings.allowed}</u>**
+    ┣**{'🫡' if usettings.edit_metadata else '🙅‍♂️'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
+    ┗**🚏 Merge mode 🚏: <u>{userMergeModeStr}</u>**
 """
         markup = b.makebuttons(
             [
