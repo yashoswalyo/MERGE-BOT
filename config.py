39feba763aeb82995df1811d5906365a03ee0192
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-10-08 22:19:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
-    IS_PREMIUM = False
+    IS_PREMIUM = True
     MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
